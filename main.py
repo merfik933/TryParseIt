577da06ec31975a8bf09b771cba04e21f5d7b886
Merfik933 <anatoliy9370@gmail.com>
2025-03-31 15:12:53 +0300

    Add short names for arguments

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser(description="Check the possibility of parsing from the site.")
     parser.add_argument("url", type=str, help="URL to parse")
-    parser.add_argument("--use-requests", type=bool, default=True, help="Use requests for parsing")
-    parser.add_argument("--use-playwright", type=bool, default=True, help="Use playwright for parsing")
+    parser.add_argument("-r", "--use-requests", type=bool, default=True, help="Use requests for parsing")
+    parser.add_argument("-p", "--use-playwright", type=bool, default=True, help="Use playwright for parsing")
 
 
 if __name__ == "__main__":
