f98c9ce15f62b2e63564693823dbd9ae2c08f93d
Merfik933 <anatoliy9370@gmail.com>
2025-03-31 16:37:50 +0300

    Fix playwright_parser.py script

--- playwright_parser.py
+++ playwright_parser.py
@@ -3,11 +3,16 @@ from playwright.sync_api import sync_playwright
 def try_parse(url):
     with sync_playwright() as p:
         try:
-            browser = p.firefox.launch(headless=True)
+            browser = p.chromium.launch(headless=False)
             page = browser.new_page()
             page.goto(url)
-            content = page.content()
-            return content
+            html = page.inner_html("html")
+
+            print(f"Successfully fetched {url} using Playwright.")
+            input("Press Enter to continue...")
+            page.close()
+
+            return html
         except Exception as e:
             print(f"Error fetching page: {e}")
             return None
