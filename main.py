81311ec4a820c881be87a1403ef2b023c6201547
Merfik933 <anatoliy9370@gmail.com>
2025-03-31 16:07:09 +0300

    Implement URL parsing with requests and playwright, saving output to HTML files

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ def main():
     parser.add_argument("-r", "--use-requests", type=bool, default=True, help="Use requests for parsing")
     parser.add_argument("-p", "--use-playwright", type=bool, default=True, help="Use playwright for parsing")
 
+    args = parser.parse_args()
 
+    url = args.url
+    if use_requests:
+        from requests_parser import try_parse
+
+        page = try_parse(url)
+        if page:
+            with open("C:\\Users\\anato\\Downloads\\page_requests.html", "w", encoding="utf-8") as file:
+                file.write(page)
+    if use_playwright:
+        import playwright_parser
+
+        page = playwright_parser.try_parse(url)
+        if page:
+            with open("C:\\Users\\anato\\Downloads\\page_playwright.html", "w", encoding="utf-8") as file:
+                file.write(page)
 if __name__ == "__main__":
     main()
